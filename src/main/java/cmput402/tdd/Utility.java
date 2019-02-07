e0fdc2781abb14f25dfce569a591061def78c842
Blaz Pocrnja <blaz@cs.ualberta.ca>
2019-02-06 17:58:43 -0700

    Added test for average score.

--- src/main/java/cmput402/tdd/Utility.java
+++ src/main/java/cmput402/tdd/Utility.java
@@ -1,18 +1,26 @@
 package cmput402.tdd;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cmput402.tdd.service.Search;
 
 public class Utility {
 
-	public HashMap<String, Integer>  countAverageScore(Search search){
+	public HashMap<String, Float>  countAverageScore(Search search){
+		HashMap<String, ArrayList<Integer>> results = search.returnAll();
 		
-		return null;
+		HashMap<String, Float> map = new HashMap<String, Float>();
+		
+		for(String player: results.keySet()){
+			Float avg = (float)results.get(player).get(1) / (float)results.get(player).get(0);
+			map.put(player, avg);
+		}
+		
+		return map;
 	}
 	
-public HashMap<String, Integer>  findTopScorer(Search search){
-		
+	public HashMap<String, Integer>  findTopScorer(Search search){
 		return null;
 	}
 	
